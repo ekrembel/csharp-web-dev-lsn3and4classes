07330b5d35f716536eb810bc89ca4f846eb36b8b
Ekrem Bel <ekrem@Ekrems-MacBook-Pro-2.local>
2020-06-24 12:21:10 -0500

    Done

--- SchoolPractice/Program.cs
+++ SchoolPractice/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace SchoolPractice
 {
@@ -7,6 +8,63 @@ namespace SchoolPractice
         public static void Main(string[] args)
         {
             // TODO: Instantiate your objects and test your exercise solutions with print statements here.
+
+            Student ekrem = new Student();
+            ekrem.Name = "Ekrem";
+            ekrem.StudentId = 10;
+            ekrem.NumberOfCredits = 1;
+            ekrem.Gpa = 4.0;
+
+            Student student = new Student();
+           
+
+            Console.WriteLine(ekrem.Name);
+            Console.WriteLine(ekrem.StudentId);
+            Console.WriteLine(ekrem.NumberOfCredits);
+            Console.WriteLine(ekrem.Gpa);
+
+            Course csharp = new Course();
+            csharp.CourseName = "C# Web Development";
+            csharp.Requirenments.Add("Commitment");
+            csharp.Requirenments.Add("Computer");
+            csharp.Requirenments.Add("internet Access");
+            csharp.Students.Add(ekrem.StudentId, ekrem.Name);
+
+            int i = 0;
+            while (i < 2)
+            {
+                Console.WriteLine("Student Name: ");
+                student.Name = Console.ReadLine();
+
+                Console.WriteLine("Student Id: ");
+                student.StudentId = int.Parse(Console.ReadLine());
+
+                csharp.Students.Add(student.StudentId, student.Name);
+                i++;
+            }
+
+            foreach (KeyValuePair<int, string> person in csharp.Students)
+            {
+                Console.WriteLine(person.Value);
+            }
+
+            foreach (string requirenment in csharp.Requirenments)
+            {
+                Console.WriteLine(requirenment);
+            }
+
+            Teacher instructor = new Teacher();
+            instructor.FirstName = "Benet";
+            instructor.LastName = "Clark";
+            instructor.Subject = "Web Development";
+            instructor.YearsTeaching = 2;
+
+            Console.WriteLine(instructor.FirstName);
+            Console.WriteLine(instructor.LastName);
+            Console.WriteLine(instructor.Subject);
+            Console.WriteLine(instructor.YearsTeaching);
+
+            
         }
     }
 }
--- SchoolPractice/Student.cs
+++ SchoolPractice/Student.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 // Start working here with your Student class.
 // To instantiate the Student class, add your code to the Main method in Program
@@ -7,9 +8,36 @@ namespace SchoolPractice
 {
     public class Student
     {
-        private string Name;
-        private int StudentId;
-        private int NumberOfCredits;
-        private double Gpa;
+        public string Name { get; set; }
+        public int StudentId { get; set; }
+        public int NumberOfCredits { get; set; }
+        public double Gpa { get; set; }
+        
+    }
+
+    public class Course
+    {
+        private Dictionary<int, string> students = new Dictionary<int, string>();
+        public List<string> requirenments = new List<string>();
+        public string CourseName { get; set; }
+        public List<string> Requirenments
+        {
+            get { return requirenments; }
+            set { requirenments = value; }
+        }
+        public Dictionary<int, string> Students
+        {
+            get { return students; }
+            set { students = value; }
+        }       
+
+    }
+
+    public class Teacher
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Subject { get; set; }
+        public int YearsTeaching { get; set; }
     }
 }
